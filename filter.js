ca1514316a9fb774e6ba849842d510cc438826a2
Tristan Timblin <tatimblin@gmail.com>
2017-03-08 09:02:41 -0500

    Moved filter js to own file.

---
+++ filter.js
@@ -0,0 +1,18 @@
+
+var acc = document.getElementsByClassName("filter-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+function clearform() {
+    document.getElementById("filter-form").reset();
+}
